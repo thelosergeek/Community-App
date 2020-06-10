4c9704e4cc42f40878fbecb4d8d7698bb8f6a339
Inam Khan <inamkh0001@gmail.com>
2020-06-10 21:25:24 +0530

    Added Profile Activity

--- app/src/main/java/in/thelosergeek/community_app/ui/ProfileActivity.java
+++ app/src/main/java/in/thelosergeek/community_app/ui/ProfileActivity.java
@@ -3,17 +3,31 @@ package in.thelosergeek.community_app.ui;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,6 +35,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -30,10 +45,13 @@ import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
 
+import java.util.HashMap;
 import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import in.thelosergeek.community_app.R;
+
+import static com.google.firebase.storage.FirebaseStorage.getInstance;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -42,13 +60,26 @@ public class ProfileActivity extends AppCompatActivity {
      FirebaseDatabase firebaseDatabase;
      DatabaseReference databaseReference;
 
+     StorageReference storageReference;
+     String storePath = "Users_Profile_Image/";
+
      CircleImageView d_image;
      TextView d_name, d_email, d_designation;
-     StorageReference imagestorage;
      ProgressDialog progressDialog;
      FloatingActionButton floatingActionButton;
 
-    private static final int imagepick = 1;
+     private static final int CAMERA_CODE = 100;
+     private static final int STORAGE_CODE = 200;
+     private static final int IMAGE_PICK_GALLERY = 300;
+     private static final int IMAGE_PICK_CAMERA = 400;
+
+     String cameraPermission[];
+     String storagePermission[];
+
+     Uri image_uri;
+
+     String ProfilePhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,16 +91,307 @@ public class ProfileActivity extends AppCompatActivity {
         d_email = (TextView) findViewById(R.id.tvemail);
         d_designation = findViewById(R.id.tvdesignation);
         floatingActionButton = (FloatingActionButton) findViewById(R.id.btnchange);
+        progressDialog = new ProgressDialog(this);
 
         // Fetching User Data
-        firebaseAuth =
-        imagestorage = FirebaseStorage.getInstance().getReference();
-        currentuser = FirebaseAuth.getInstance().getCurrentUser();
-        String current_uid = currentuser.getUid();
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
-        databaseReference.keepSynced(true);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("Users");
+        storageReference = getInstance().getReference();
 
 
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+
+
+        Query query = databaseReference.orderByChild("email").equalTo(firebaseUser.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    String name = "" + ds.child("name").getValue();
+                    String email = "" + ds.child("email").getValue();
+                    String image = "" + ds.child("image").getValue();
+
+
+                    d_name.setText(name);
+                    d_email.setText(email);
+                    try {
+                        Picasso.get().load(image).into(d_image);
+                    } catch (Exception e) {
+                        Picasso.get().load(R.drawable.defaultpic).into(d_image);
+                    }
+
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditProfile();
+            }
+        });
+    }
+    private boolean checkStoragePermission(){
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_CODE);
+    }
+
+    private boolean checkCameraPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode== RESULT_OK){
+            if(requestCode == IMAGE_PICK_GALLERY){
+                image_uri = data.getData();
+                uploadProfilePhoto(image_uri);
+            }
+            if(requestCode == IMAGE_PICK_CAMERA){
+                uploadProfilePhoto(image_uri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadProfilePhoto(final Uri uri) {
+
+        progressDialog.show();
+        String filePath = storePath+ ""+ProfilePhoto + ""+ firebaseUser.getUid();
+        StorageReference storageReference2 = storageReference.child(filePath);
+        storageReference2.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isSuccessful());
+                Uri downloadUri = uriTask.getResult();
+
+                if(uriTask.isSuccessful()){
+                    HashMap<String, Object> results = new HashMap<>();
+                    results.put(ProfilePhoto, downloadUri.toString());
+
+                    databaseReference.child(firebaseUser.getUid()).updateChildren(results)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(ProfileActivity.this,"Updated",Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ProfileActivity.this,"Error Occured",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                else{
+                    progressDialog.dismiss();
+                    Toast.makeText(ProfileActivity.this,"Error Occured", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                progressDialog.dismiss();
+                Toast.makeText(ProfileActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void EditProfile() {
+        String options[] = {"Edit Profile Picture", "Edit Name"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which==0){
+                progressDialog.setMessage("Update Image");
+                ProfilePhoto = "image";
+                showProfilePicDialog();
+                }
+                else if(which==1){
+                    progressDialog.setMessage("Update Name");
+                    showNameUpdateDialog("name");
+
+                }
+
+            }
+        });
+        builder.create().show();
+    }
+
+    private void showNameUpdateDialog(final String key) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Update" + key);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(10,10,10,10);
+
+        final EditText editText = new EditText(this);
+        editText.setHint("Enter" + key);
+        linearLayout.addView(editText);
+
+        builder.setView(linearLayout);
+
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String value = editText.getText().toString().trim();
+                if(!TextUtils.isEmpty(value)){
+                    progressDialog.show();
+                    HashMap<String, Object> result = new HashMap<>();
+                    result.put(key,value);
+
+                    databaseReference.child(firebaseUser.getUid()).updateChildren(result)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(ProfileActivity.this,"Updated" , Toast.LENGTH_SHORT).show();
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ProfileActivity.this,"" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    });
+
+
+                }
+                else{
+                    Toast.makeText(ProfileActivity.this,"Enter" + key, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            progressDialog.dismiss();
+            }
+        });
+
+        builder.create().show();
+
+
+    }
+
+    private void showProfilePicDialog() {
+
+        String options[] = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick From");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which==0){
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    }
+                    else {
+                        PickFromCamera();
+                    }
+                }
+                else if(which==1){
+                    if(!checkStoragePermission()){
+                        requestStoragePermission();
+                    }
+                    else{
+                        PickFromGallery();;
+                    }
+
+                }
+
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case CAMERA_CODE:{
+                if(grantResults.length>0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted&& writeStorageAccepted){
+                        PickFromCamera();
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"Please enable camera permission", Toast.LENGTH_SHORT).show();
+                    }
+                 }
+            }
+            break;
+            case STORAGE_CODE:{
+                if(grantResults.length>0){
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(writeStorageAccepted){
+                        PickFromGallery();
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"Please enable storage permission", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    private void PickFromGallery() {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE,"TEMP PIC");
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION,"TEMP DESC");
+        image_uri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(intent, IMAGE_PICK_CAMERA);
+    }
+
+    private void PickFromCamera() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY);
+        
+    }
 
 
 }
